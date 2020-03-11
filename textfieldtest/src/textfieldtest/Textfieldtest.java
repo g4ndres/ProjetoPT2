9f377a7a955a68f12b357c1d71931623493cfc42
g4ndres <60987508+g4ndres@users.noreply.github.com>
2020-03-11 18:22:31 -0400

    Add files via upload

---
+++ textfieldtest/src/textfieldtest/Textfieldtest.java
@@ -0,0 +1,25 @@
+package textfieldtest;
+
+import javax.swing.JFrame;
+
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Textfieldtest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        textfieldframe textFieldFrame = new textfieldframe();
+        textFieldFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        textFieldFrame.setSize( 350,100 );
+        textFieldFrame.setVisible( true );
+         
+    }
+    
+}
